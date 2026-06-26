bb03622a130dc23ca9bd730d27863687603a7974
jiyujin <jiyujin816@gmail.com>
2026-06-26 13:22:10 +0900

    Update script.js

--- script.js
+++ script.js
@@ -4,18 +4,13 @@ const preview=document.getElementById("preview");
 // 数式を更新
 async function update(){
 
-    // HTMLとして表示
-    preview.innerHTML=input.value;
+    // 入力を自動でディスプレイ数式として扱う
+    preview.innerHTML="$$\n"+input.value+"\n$$";
 
-    // MathJaxがまだ読み込まれていないなら何もしない
     if(!window.MathJax)return;
 
-    // 前回の数式を消す
     MathJax.typesetClear([preview]);
-
-    // 再レンダリング
     await MathJax.typesetPromise([preview]);
 }
 
-// 入力のたび更新
 input.addEventListener("input",update);
